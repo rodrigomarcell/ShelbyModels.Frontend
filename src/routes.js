739f662e091d686e087297007d7b642d50923312
Rodrigo Marcel <rodrigomarcell@outlook.com>
2025-06-22 21:52:01 -0400

    Remoção de arquivos desnecessários, incluindo componentes, rotas, estilos e imagens, para simplificar a estrutura do projeto.

--- src/routes.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-const Home = React.lazy(() => import('./views/Home'))
-
-const routes = [
-  { path: '/', exact: true, name: 'Home', element: Home },
-]
-
-export default routes
